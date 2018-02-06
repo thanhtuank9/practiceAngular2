709911809d53dd26ec7c117026bb994a2aa38882
Tuan Nguyen <tuan.nguyen@dispatchingsolutions.com>
2018-02-06 15:03:12 +0700

    binding data bewteen component and html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 --1. Start app: 
 ng serve --open
+
+--2. create component
+ng g c [component name] or ng generate component [component name]
+
---
+++ learnAngular5/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = 'Add Item';
+  goalText: string = 'My first life goal';
+  goals = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    if(!this.goalText){
+      alert('Please enter goal!');
+      return;
+    }
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+
+}
