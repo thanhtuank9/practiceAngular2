4809fb47cd895a1ed68f1e1cec0c44e0d4493f8a
Tuan Nguyen <tuan.nguyen@dispatchingsolutions.com>
2018-02-08 17:30:08 +0700

    Add Router

--- learnAngular5/src/app/about/about.component.ts
+++ learnAngular5/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { 
+
+    this.route.params.subscribe(res => console.log(res.id));
+
+  }
 
   ngOnInit() {
   }
